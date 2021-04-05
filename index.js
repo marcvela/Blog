d4022966a5548086761d50f7107110cb4115dbfe
marcvela <>
2021-04-05 11:14:10 +0000

    still can't get code to present on same page. Reverted to working code to at least display from input box

--- index.js
+++ index.js
@@ -37,19 +37,6 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-// need to write out an app.get function that open a route /weather Need an EJS view called weather.ejs that displays one text field to input city name
-app.get("/weather", function(req, res) {
-  res.render("weather", {weatherContent: weatherContent});
-});
-app.post("/weather", function(req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-  weatherPost.push(post);
-  res.redirect("/weather");
-});
-
 app.get("/compose", function(req, res){
   res.render("compose");
 });
@@ -87,34 +74,40 @@ app.get("/posts/:postName", function(req, res){
 // Then need to write out an app.get function that will use the city name to query the Weather API to retrieve basic weather information - temperature, description and humidity
 // The display of the weather information must be saved to an array and then the results of the array must be pushed to the /weather EJS view to display
 // The /weather route and page created by weather.ejs page should allow for the input of the city name, and the display of the weather for the city - city name, weather icon image, temperature in F, description, humidity, wind direction
-  var cityID = String(req.body.cityIDInput);
-  console.log(req.body.cityIDInput)
+
+
+// need to write out an app.get function that open a route /weather Need an EJS view called weather.ejs that displays one text field to input city name
+
+app.get("/weather", function(req, res) {
+res.render("weather", {weatherContent: weatherContent, posts:posts});
+});
+
+app.post("/weather", function(req, res) {
+  var city = String(req.body.cityInput);
   const units = "imperial";
   const apiKey = "2772dd5b7e80a5b58de7f2713214959c";
-  const url = "https://api.openweathermap.org/data/2.5/weather?id=" + cityID + "&units=" + units + "&APPID=" + apiKey;
-
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&APPID=" + apiKey;
   https.get(url, function(response) {
-    console.log(response.statusCode);
-
     response.on("data", function(data) {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const city = weatherData.name;
-      const wind = weatherData.wind.speed;
-      const humidity = weatherData.main.humidity;
-      const clouds = weatherData.clouds.all;
-      const windDir = weatherData.wind.deg;
-      const weatherDescription = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const city = weatherData.name;
+        const wind = weatherData.wind.speed;
+        const humidity = weatherData.main.humidity;
+        const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-      res.render("<h1> The weather is " + weatherDescription + ".<h1>");
-      res.render("<h2>The Temperature in " + city + " " + " is " + temp + "°F and the wind speed is " + wind + "MPH. The wind direction is " + windDir + "° and the cloud level is " + clouds + "%.<h2>");
-      res.render("<img src=" + imageURL + ">");
+        res.write("<h1> The weather is " + weatherDescription + "<h1>");
+        res.write("<h2>The Temperature in " + city + " " + " is " + temp + "°F and the wind speed is " + wind + "MPH with a current humidity of " + humidity + "%. <h2>");
+        res.write("<img src=" + imageURL +">");
+        
+        res.send();
+        
+      });
     });
-  });
-
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
-});
+})
